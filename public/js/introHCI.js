c73b35306228a9c731e8c0b94615789b102233ec
Fiona Feng <ffeng@ucsd.edu>
2021-02-18 17:25:30 -0800

    Google Analytics Exercise

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,10 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+ $('.likeBtn').click(clickLikeBtn);
+}
+ 
+function clickLikeBtn(e) {
+ e.preventDefault();
+ ga('send', 'event', 'like', 'click');
 }
